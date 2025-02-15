56a2141808ed699421f784711df7db3127ae260c
Jonathan W. <150624002+marte2050@users.noreply.github.com>
2025-02-15 12:20:37 -0300

    :ok_hand: Style - ajuste do estilo do código com base no ruff.

--- iam/config/urls.py
+++ iam/config/urls.py
@@ -3,6 +3,6 @@ from django.urls import include, path
 from oauth2_provider import urls as oauth2_urls
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('o/', include(oauth2_urls)),
+    path("admin/", admin.site.urls),
+    path("o/", include(oauth2_urls)),
 ]
--- iam/users/admin/user_admin.py
+++ iam/users/admin/user_admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
+
 from users.models import User
 
+
 @admin.register(User)
-class UserAdmin(admin.ModelAdmin): ...
+class UserAdmin(admin.ModelAdmin):
+    """Interface para administração do modelo de dados User."""
