ad9b62b626bd04133264c12985494660b66feaba
Jonathan W. <150624002+marte2050@users.noreply.github.com>
2025-02-15 21:34:47 -0300

    :bug: Fix - remove dos fieldsets as opções da data de criação do usuário.

--- iam/users/admin/user_admin.py
+++ iam/users/admin/user_admin.py
@@ -1,8 +1,31 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as UserAdminBase
+from django.utils.translation import gettext_lazy as _
 
 from users.models import User
 
 
 @admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    """Interface para administração do modelo de dados User."""
+class UserAdmin(UserAdminBase):
+    """Classe de administração de usuários.
+
+    Os fieldsets para edição de usuário são construídos por meio do
+    método _changeform_view da classe pai.
+    """
+
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name", "email")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+    )
